664b62b8a8fbed976171b730be8b1ea4de85e461
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-13 09:30:03 +0700

    basic io with cpp

---
+++ integerkali.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "input a: ";
+    cin >> a;
+    cout << "input b: ";
+    cin >> b;
+    cout << "hasil: " << a * b << endl;
+    return 0;
+}
